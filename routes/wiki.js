41704e1c53f6453056d2ac2798591eaf970b37e1
alexander-hp <hdezppalex@gmail.com>
2024-02-21 16:17:34 -0600

    first commit

---
+++ routes/wiki.js
@@ -0,0 +1,25 @@
+// wiki.js - Wiki route module.
+
+const express = require('express');
+const router = express.Router();
+
+// Home page route.
+router.get('/', function (req, res) {
+  res.send('Wiki home page');
+});
+
+// About page route.
+router.get('/about', async function (req, res, next) {
+  try {
+    const successfulResult = await About.find({}).exec();
+    res.render('about_view', { title: 'About', list: successfulResult });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.post('/about', (req, res) => {
+  res.send('About this wiki');
+});
+
+module.exports = router;
