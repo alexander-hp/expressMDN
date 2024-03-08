0573e0a33f64a672a2d690211ea19e24d8980564
alexander-hp <hdezppalex@gmail.com>
2024-03-08 10:48:53 -0600

    rm compression

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
-const compression = require("compression");
+// const compression = require("compression");
 const helmet = require("helmet");
 // Set up rate limiter: maximum of twenty requests per minute
 const RateLimit = require("express-rate-limit");
@@ -31,8 +31,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(compression()); // Compress all routes
-app.use(express.static(path.join(__dirname, "public")));  
+// app.use(compression()); // Compress all routes
+// app.use(express.static(path.join(__dirname, "public")));  
+
 // Add helmet to the middleware chain.
 // Set CSP headers to allow our Bootstrap and Jquery to be served
 app.use(
